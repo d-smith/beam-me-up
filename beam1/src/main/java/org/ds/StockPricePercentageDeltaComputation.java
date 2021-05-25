4dfd8901280995f96bd49dc00490fc4d5d9974f7
d-smith <doug.smith.mail@gmail.com>
2021-05-25 14:20:27 -0700

    Finished stock price example

--- beam1/src/main/java/org/ds/StockPricePercentageDeltaComputation.java
+++ beam1/src/main/java/org/ds/StockPricePercentageDeltaComputation.java
@@ -1,9 +1,14 @@
 package org.ds;
 
-import org.apache.beam.sdk.options.Default;
-import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Watch;
+import org.apache.beam.sdk.values.KV;
+import org.joda.time.Duration;
+
 
 public class StockPricePercentageDeltaComputation {
     private static final String CSV_HEADER=
@@ -22,5 +27,68 @@ public class StockPricePercentageDeltaComputation {
         String getOutputFile();
 
         void setOutputFile(String value);
+    }
+
+    public static void main(String... args) {
+        ComputationOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+                .as(ComputationOptions.class);
+
+        Pipeline pipeline = Pipeline.create(options);
+
+        pipeline.apply(TextIO.read().from(options.getInputFile())
+                .watchForNewFiles(Duration.standardSeconds(10),
+                        Watch.Growth.afterTimeSinceNewOutput(Duration.standardSeconds(30))))
+                .apply(ParDo.of(new FilterHeaderFn(CSV_HEADER)))
+                .apply(ParDo.of(new ComputePriceDeltaPercentage()))
+                .apply(ParDo.of(new ConvertToStringFn()))
+                .apply(ParDo.of(new DoFn<String, Void>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        System.out.println(c.element());
+                    }
+                }));
+
+        pipeline.run().waitUntilFinish();
+
+    }
+
+    public static class FilterHeaderFn extends DoFn<String,String>{
+        private final String header;
+
+        public FilterHeaderFn(String header) {
+            this.header = header;
+        }
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String row = c.element();
+
+            if(!row.isEmpty() && !row.equals(this.header)) {
+                c.output(row);
+            }
+        }
+    }
+
+    private static class ComputePriceDeltaPercentage extends  DoFn<String, KV<String,Double>> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String data[] = c.element().split(",");
+
+            String date = data[0];
+            double openPrice = Double.parseDouble(data[1]);
+            double closePrice = Double.parseDouble(data[4]);
+
+            double percentageDelta = ((closePrice - openPrice)/openPrice) * 100;
+            Double percentageDeltaRounded = Math.round(percentageDelta * 100.0) / 100.0;
+
+            c.output(KV.of(date,percentageDeltaRounded));
+        }
+    }
+
+    public static class ConvertToStringFn extends DoFn<KV<String,Double>, String> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            c.output(c.element().getKey() + "," + c.element().getValue());
+        }
     }
 }
